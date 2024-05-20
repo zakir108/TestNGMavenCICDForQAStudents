f2c6726a5eb91a3282de5fe1b35ffa0d9e7d20bf
zakir108 <zakir108@gmail.com>
2024-05-20 16:55:22 -0700

    Minor

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -26,45 +26,18 @@ public class Main {
 
 //================================== zakir pc, MY CODING======================
 
-
-
-
     /* WRITTEN ONLY BY zAKIR */
     public static int Fibonacci(int x,int y)
     {
          int z =x*y; 
         return z;
-    }
-    // 5! = 5*4*3*2*1 = 120
-    // 6! = 6*5*3*2* = 720
+    }   
     public static int Factorial(int input)// added by zakir
-    {
-        ///
-        ///
-
-        ///
-
-
-
-
-
-
-
-
-
-
-
-        
-        //
+    {       
         return 0;
     }
     public static int Factorial1(int input)// added by zakir
     {
-        ///
-        ///
-        ///
-        //
         return 0;
-    }
-    
+    }    
 }
